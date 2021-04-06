bad0f41dc6fd151c474a3c6bc2490af5730d61a6
Katerine Martinez <lp.kathe@gmail.com>
2021-04-05 19:08:17 -0500

    fix layout basic

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,25 @@ const btnCart = document.getElementById("header_cart");
 
 const viewProducts = document.getElementById("products");
 
-function cartPage (event) {
- viewCart.classList.toggle("cart_active");
- viewProducts.classList.toggle("products_inactive");
+const modelCard = document.getElementById("card");
+const fileJson = function () {
+  fetch('products.json')
+    .then(response => response.json())
+    .then(json => console.log(json))
+};
+
+/**
+ * Show and keep products and cart view.
+ * @param {click} event 
+ */
+function cartPage(event) {
+  viewCart.classList.toggle("cart_active");
+  viewProducts.classList.toggle("products_inactive");
+};
+
+function createCards(json) {
+  const clonedCard = modelCard.cloneNode(true);
+  viewProducts.appendChild(clonedCard);
 };
 
 btnCart.addEventListener("click", cartPage);
