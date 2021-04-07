b973acc565a1b72cc9e6ff91f8cb6c2ec969c0bb
Katerine Martinez <lp.kathe@gmail.com>
2021-04-06 23:02:19 -0500

    Add shopping cart counter

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,23 @@
 const viewCart = document.getElementById("cart");
-const btnCart = document.getElementById("header_cart");
+const btnCart = document.getElementById("headerCart");
 
+const cartCounter = document.getElementById("cartCounter");
 const viewProducts = document.getElementById("products");
 
 const modelCard = document.getElementById("card");
-const fileJson = function () {
-  fetch('products.json')
+let stockProducts = [];
+
+let shoppingCart = [];
+
+const loadInfo = function () {
+  fetch('scripts/products.json')
     .then(response => response.json())
-    .then(json => console.log(json))
+    .then(json => {
+      json.products.forEach((element, index) => createCards(element, index));
+      stockProducts = json.products;
+    })
 };
+loadInfo()
 
 /**
  * Show and keep products and cart view.
@@ -19,9 +28,47 @@ function cartPage(event) {
   viewProducts.classList.toggle("products_inactive");
 };
 
-function createCards(json) {
+/**
+ * Clone a card model for each product within the JSON file.
+ * @param {JSON Objet} element 
+ */
+function createCards(element, index) {
   const clonedCard = modelCard.cloneNode(true);
   viewProducts.appendChild(clonedCard);
+  const desiredQuantity = clonedCard.querySelector(".desiredQuantity");
+  desiredQuantity.setAttribute("max", `${element.stock}`);
+  clonedCard.setAttribute("id", index);
+  //desiredQuantity.setAttribute("oninput", "validity.valid||(value='')");
+  clonedCard.querySelector(".btnAddCart").addEventListener("click", addToCart);
+  clonedCard.style.display = "block";
+};
+
+function addToCart(event) {
+  const item = event.target.closest("div");
+  const productNumber = item.id;
+  const product = stockProducts[productNumber];
+  const quantity = parseInt(item.querySelector(".desiredQuantity").value);
+
+  const productInCart = shoppingCart.filter(data => data.product == product);
+
+  if (productInCart.length == 0) {
+    if (quantity <= product.stock) {
+      const itemCart = {
+        "product": product,
+        "quantity": quantity,
+      };
+      shoppingCart.push(itemCart);
+    } else {
+      alert(`In stock ${product.stock} products only`);
+    };
+  } else {
+    if ((productInCart[0].quantity + quantity) <= product.stock) {
+      productInCart[0].quantity += quantity;
+    } else {
+      alert(`You can add ${(productInCart[0].quantity + quantity) - product.stock} more items.`);
+    };
+  };
+  cartCounter.innerText = shoppingCart.length
 };
 
 btnCart.addEventListener("click", cartPage);
